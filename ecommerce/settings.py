2e200d927151ad77807d26a9ce690c36b7b982cb
Francisco Junior <franciscojn49@gmail.com>
2019-12-11 15:27:41 -0300

    kkkdf

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -118,7 +118,7 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
+# Static files (CSS, JavaScript, Images )
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
