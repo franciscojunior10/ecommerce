f0cd1767ff145c80b5177aebcedc63aec516fbab
Francisco Junior <franciscojn49@gmail.com>
2019-12-11 14:44:39 -0300

    adicionando testes e heroku

---
+++ core/tests/test_views.py
@@ -0,0 +1,16 @@
+from django.test import TestCase, Client
+from django.core.urlresolvers import reverse
+
+class IndexViewTestCase(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.url = reverse('index')
+
+    def test_status_code(self):
+        response = self.client.get(self.url)
+        self.assertEquals(response.status_code, 200)
+
+    def test_template_used(self):
+        response = self.client.get(self.url)
+        self.assertTemplateUsed(response, 'index.html')
