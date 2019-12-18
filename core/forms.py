55d704e9724e54a12faa63ac5c7fbd8d3714619c
Francisco Junior <franciscojn49@gmail.com>
2019-12-18 09:38:40 -0300

    created form in app core

---
+++ core/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+
+class ContactForm(forms.Form):
+
+    name = forms.CharField(label='Nome')
+    email = forms.EmailField(label='E-mail')
+    message = forms.CharField(label='Mensagem', widget=forms.Textarea)
